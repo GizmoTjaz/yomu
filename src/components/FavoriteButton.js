a6b8881247a8196b077c63d43cdcf0b36120be25
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-11-12 13:54:49 +0100

    implement favoriting/unfavoriting manga

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -1,34 +1,33 @@
 // Modules
 import React from "react";
 import * as Haptics from "expo-haptics";
+import { connect } from "react-redux";
 
 // Components
 import RoundIconButton from "./RoundIconButton";
 
-// Contexts
-import FavoriteManga from "../stores/FavoriteManga";
-
-export default function FavoriteButton ({ manga }) {
-    
-    const
-        { favorites, addFavorite, removeFavorite } = FavoriteManga(),
-        isFavorite = favorites.hasOwnProperty(manga.id),
-        [ _, forceRender ] = React.useState({});
-
+function FavoriteButton ({ manga, favoriteManga, toggleFavorite }) {
     return (
         <RoundIconButton
-            icon = { isFavorite ? "ios-heart" : "ios-heart-empty" }
+            icon = { favoriteManga.hasOwnProperty(manga.id) ? "ios-heart" : "ios-heart-empty" }
             onPress = { () => {
 
                 Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
-
-                isFavorite
-                ? removeFavorite(manga)
-                : addFavorite(manga)
-
-                forceRender({});
+                toggleFavorite(manga.getHusk());
 
             } }
         />
     );
 }
+
+function stateMap ({ favoriteManga }) {
+    return { favoriteManga };
+}
+
+function dispatchMap (dispatch) {
+    return {
+        toggleFavorite: (manga) => dispatch({ type: "TOGGLE_FAVORITE_MANGA", payload: manga })
+    };
+}
+
+export default connect(stateMap, dispatchMap)(FavoriteButton);
