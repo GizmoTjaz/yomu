dd330ba1910c5dc374fed901e4c547b1ab3c7801
Tjaz <25076630+GizmoTjaz@users.noreply.github.com>
2020-11-12 12:00:19 +0100

    add toggle opacity to reader UI

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -1,11 +1,13 @@
 // Modules
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Alert } from "react-native";
 import { setStatusBarHidden } from "expo-status-bar";
 
 // Components
-import { Modal, SafeAreaView, View, Text, ActivityIndicator, Animated, TouchableWithoutFeedback } from "react-native";
+import { Modal, SafeAreaView, View, Text, ActivityIndicator, Animated } from "react-native";
 import RoundIconButton from "./RoundIconButton";
 import ReaderPageCarousel from "./ReaderPageCarousel";
+import { TapGestureHandler, State } from "react-native-gesture-handler";
 
 // Styles
 import styles from "../styles/Reader";
@@ -23,30 +25,43 @@ export default function Reader ({ data: { manga, chapterIndex }, onClose }) {
         [ overlayVisibility, setOverlayVisibility ] = useState(1),
         [ pageIndex, setPageIndex ] = useState(0);
 
-    const opacityValue = new Animated.Value(1);
+    const
+        opacityValue = useRef(new Animated.Value(1)).current,
+        opacityState = useRef(true);
 
     useEffect(() => {
         if (manga && manga.chapters) {
+
+            setChapter(null);
+
             getChapter(manga.chapters[chapterIndex].id).then(chapter => {
                 setChapter(chapter);
                 setPageIndex(0);
             }).catch(err => {
-                console.error(err);
+                Alert.alert("Error", "Couldn't fetch chapter pages.", [
+                    {
+                        text: "OK",
+                        onPress: onClose
+                    }
+                ]);
             });
         }
     }, [ manga, chapterIndex ]);
 
-    useEffect(() => {
+    function tapGestureEvent (event) {
+        if (event.nativeEvent.state === State.END) {
 
-        setStatusBarHidden(!overlayVisibility, "fade");
+            opacityState.current = !opacityState.current;
+            setStatusBarHidden(!opacityState.current, "fade");
 
-        Animated.timing(opacityValue, {
-            toValue: overlayVisibility ? 1 : 0,
-            duration: 230,
-            useNativeDriver: true
-        }).start();
+            Animated.timing(opacityValue, {
+                toValue: opacityState.current ? 1 : 0,
+                duration: 230,
+                useNativeDriver: true
+            }).start();
 
-    }, [ overlayVisibility ]);
+        }
+    }
 
     return (
         <Modal
@@ -54,74 +69,77 @@ export default function Reader ({ data: { manga, chapterIndex }, onClose }) {
             visible = { !!manga }
             presentationStyle = { "fullScreen" }
         >
-            <View style = {{ ...styles.container, ...(chapter ? {} : styles.loadingContainer) }}>
-                {
-                    chapter ? (
-                        <SafeAreaView style = { styles.safeAreaContainer }>
-                            <Animated.View
-                                style = {{
-                                    ...styles.floatingContainer,
-                                    ...styles.topContainer,
-                                    opacity: opacityValue
-                                }}
-                            >
-                                <RoundIconButton
-                                    icon = "ios-cog"
-                                    buttonStyle = { styles.hoverButton }
-                                />
-                                <View style = {{ ...styles.hoverBox, ...styles.titleContainer }}>
-                                    <Text style = { styles.volumeChapterLabel }>
-                                        { `${
-                                            chapter.volume
-                                            ? `Volume ${ chapter.volume }, `
-                                            : ""
-                                        }Chapter ${ chapter.chapter }` }
-                                    </Text>
-                                    <Text style = { styles.titleLabel }>{ chapter.title }</Text>
+            <TapGestureHandler
+                numberOfTaps = { 1 }
+                maxDelayMs = { 0 }
+                onHandlerStateChange = { tapGestureEvent }
+            >
+                <View style = {{ ...styles.container, ...(chapter ? {} : styles.loadingContainer) }}>
+                    {
+                        chapter ? (
+                            <SafeAreaView style = { styles.safeAreaContainer }>
+                                <Animated.View
+                                    style = {{
+                                        ...styles.floatingContainer,
+                                        ...styles.topContainer,
+                                        opacity: opacityValue
+                                    }}
+                                >
+                                    <RoundIconButton
+                                        icon = "ios-cog"
+                                        buttonStyle = { styles.hoverButton }
+                                    />
+                                    <View style = {{ ...styles.hoverBox, ...styles.titleContainer }}>
+                                        <Text style = { styles.volumeChapterLabel }>
+                                            { `${
+                                                chapter.volume
+                                                ? `Volume ${ chapter.volume }, `
+                                                : ""
+                                            }Chapter ${ chapter.chapter }` }
+                                        </Text>
+                                        <Text style = { styles.titleLabel }>{ chapter.title }</Text>
+                                    </View>
+                                    <RoundIconButton
+                                        icon = "ios-close"
+                                        size = { 30 }
+                                        buttonStyle = { styles.hoverButton }
+                                        onPress = { () => onClose() }
+                                    />
+                                </Animated.View>
+                                <View style = {{ ...styles.floatingContainer, ...styles.pageContainer }}>
+                                    <ReaderPageCarousel
+                                        style = { styles.pageImage }
+                                        pages = { chapter.pages }
+                                        onPageChange = { index => setPageIndex(index) }
+                                    />
                                 </View>
-                                <RoundIconButton
-                                    icon = "ios-close"
-                                    size = { 30 }
-                                    buttonStyle = { styles.hoverButton }
-                                    onPress = { () => onClose() }
-                                />
-                            </Animated.View>
-                            <View style = {{ ...styles.floatingContainer, ...styles.pageContainer }}>
-                                <ReaderPageCarousel
-                                    style = { styles.pageImage }
-                                    pages = { chapter.pages }
-                                    onPageChange = { index => setPageIndex(index) }
-                                />
-                            </View>
-                            <Animated.View
-                                style = {{
-                                    ...styles.floatingContainer,
-                                    ...styles.bottomContainer,
-                                    opacity: opacityValue
-                                }}
-                            >
-                                <View style = {{ ...styles.hoverBox, ...styles.pageCounterContainer }}>
-                                    <Text style = { styles.pageCounterLabel }>
-                                        { `${Math.max(1, pageIndex + 1)} / ${ chapter.pages.length }` }
-                                    </Text>
-                                </View>
-                            </Animated.View>
-                        </SafeAreaView>
-                    ) : (
-                        <ActivityIndicator
-                            size = "large"
-                            color = { COLORS.text }
-                        />
-                    )
-                }
-            </View>
+                                <Animated.View
+                                    style = {{
+                                        ...styles.floatingContainer,
+                                        ...styles.bottomContainer,
+                                        opacity: opacityValue
+                                    }}
+                                >
+                                    <View style = {{ ...styles.hoverBox, ...styles.pageCounterContainer }}>
+                                        <Text style = { styles.pageCounterLabel }>
+                                            { `${Math.max(1, pageIndex + 1)} / ${ chapter.pages.length }` }
+                                        </Text>
+                                    </View>
+                                </Animated.View>
+                            </SafeAreaView>
+                        ) : (
+                            <ActivityIndicator
+                                size = "large"
+                                color = { COLORS.text }
+                            />
+                        )
+                    }
+                </View>
+            </TapGestureHandler>
         </Modal>
     );
 };
 
-/*
-
-*/
 
 /*
 
